9dfff6efcb116b90fb2dfbfa5ea15fe887581271
AppleLee0423 <ld210@naver.com>
2022-11-09 15:13:14 +0900

    http://localhost:8080/ connect

--- src/main/java/com/spring/uhdiya/test.java
+++ src/main/java/com/spring/uhdiya/test.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class test {
 
-	@GetMapping("/main")
+	@GetMapping("/")
 	public String home() {
 		return "index";
 	}
